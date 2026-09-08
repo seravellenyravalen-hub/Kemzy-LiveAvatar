6ae30dda58fcbb3eb9939a6ed83cca7904db54bc
seravellenyravalen-hub <seravellenyravalen@gmail.com>
2026-09-09 00:43:20 +0100

    feat: extend tracking result with expressions

--- app/src/main/java/com/kemzy/liveavatar/FaceTrackingResult.kt
+++ app/src/main/java/com/kemzy/liveavatar/FaceTrackingResult.kt
@@ -8,7 +8,10 @@ data class FaceTrackingResult(
     val centerX: Float = 0f,
     val centerY: Float = 0f,
     val width: Float = 0f,
-    val height: Float = 0f
+    val height: Float = 0f,
+    val leftEyeOpenProbability: Float? = null,
+    val rightEyeOpenProbability: Float? = null,
+    val smilingProbability: Float? = null
 ) {
     companion object {
         fun none() = FaceTrackingResult(faceCount = 0)
