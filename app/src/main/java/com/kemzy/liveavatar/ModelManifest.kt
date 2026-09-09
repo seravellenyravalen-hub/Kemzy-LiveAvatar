fe4531a390a0a5552908ef779afe79b2d50a99a2
seravellenyravalen-hub <seravellenyravalen@gmail.com>
2026-09-09 12:19:50 +0100

    Remove unused EMAP and expression models from Android runtime

--- app/src/main/java/com/kemzy/liveavatar/ModelManifest.kt
+++ app/src/main/java/com/kemzy/liveavatar/ModelManifest.kt
@@ -20,23 +20,11 @@ object FaceModelManifest {
             fileName = "inswapper_128.onnx",
             required = true,
             remoteUrl = "https://huggingface.co/leonelhs/insightface/resolve/main/inswapper_128.onnx"
-        ),
-        FaceModelDescriptor(
-            id = "inswapper-emap",
-            fileName = "emap.bin",
-            required = true,
-            remoteUrl = "https://raw.githubusercontent.com/Parasaran-Python/android-face-fusion/master/app/src/main/assets/emap.bin"
         )
     )
 
-    val optional = listOf(
-        FaceModelDescriptor(
-            id = "expression-restorer",
-            fileName = "expression-restorer.onnx",
-            required = false
-        )
-    )
+    val optional = emptyList<FaceModelDescriptor>()
 
     val all: List<FaceModelDescriptor>
-        get() = required + optional
+        get() = required
 }
