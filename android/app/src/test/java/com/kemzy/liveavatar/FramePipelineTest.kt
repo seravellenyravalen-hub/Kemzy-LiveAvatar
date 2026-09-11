9df37197617c6e3483bdff52c2a48a9b896309b2
seravellenyravalen-hub <seravellenyravalen@gmail.com>
2026-09-11 12:11:06 +0100

    test(android): test frame pipeline without Android Bitmap

--- android/app/src/test/java/com/kemzy/liveavatar/FramePipelineTest.kt
+++ android/app/src/test/java/com/kemzy/liveavatar/FramePipelineTest.kt
@@ -1,18 +1,14 @@
 package com.kemzy.liveavatar
 
-import android.graphics.Bitmap
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class FramePipelineTest {
     @Test
-    @Suppress("UNCHECKED_CAST")
     fun latestFrameWinsWhenConsumerFallsBehind() {
-        val pipeline = FramePipeline(capacity = 1)
-        val first = null as Bitmap
-        val second = null as Bitmap
-        pipeline.offer(Frame(1, first))
-        pipeline.offer(Frame(2, second))
-        assertEquals(2, pipeline.poll()?.id)
+        val pipeline = FramePipeline<Int>(capacity = 1)
+        pipeline.offer(1)
+        pipeline.offer(2)
+        assertEquals(2, pipeline.poll())
     }
 }
